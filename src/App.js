470bd4f1b875e7329c3745c41f75189a1092eb60
sarah-am <sarahq.aman@gmail.com>
2019-10-27 18:12:38 +0300

    Tasks list

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    tasks: [
+      {
+        title: "Eat a banana",
+        details: "Find a banana. Eat it."
+      },
+      {
+        title: "Tell The Monkey to get off his monkey butt and do something.",
+        details: ""
+      }
+    ]
+  };
+
+  render() {
+    let tasks_list = this.state.tasks.map(task => (
+      <p>
+        {task.title} - {task.details}
+      </p>
+    ));
+    return <div className="App">{tasks_list}</div>;
+  }
+}
+
+export default App;
